484b1e4ffa068a88d45596e133702763ddfd8037
Jaqueline Kwiasowski <jaqueline@freeletics.com>
2017-10-30 15:32:43 +0100

    moved functions to library

--- Generator/Library.fs
+++ Generator/Library.fs
@@ -1,5 +1,17 @@
+
 namespace Generator
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open MathNet.Numerics.Statistics;
+open MathNet.Numerics.Distributions;
+
+
+type Athlete = {firstName : string; age : int; height : double; weight : double }
+
+
+module Users =
+  let names = [|"Bob";"Ali";"Lisa";"Peter";"Hanna";"Anna";"James";"Jennifer";"Sarah";"Max";"Hugo"|]
+  let genUser _ = {firstName = names.[System.Random().Next(0,10)]; age = int(Normal.Sample(30.00,4.00)); height = Normal.Sample(175.00 , 5.0); weight = Normal.Sample(75.00,5.00)}  
+  let createUser n =
+    [1..n] |> List.map genUser
+      
+     
--- NutritionUsers/Program.fs
+++ NutritionUsers/Program.fs
@@ -1,19 +1,7 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-open MathNet.Numerics.Statistics;
-open MathNet.Numerics.Distributions;
-
-type Athlete = {firstName : string; age : int; height : double; weight : double }
-
-let names = [|"Bob";"Ali";"Lisa";"Peter";"Hanna";"Anna";"James";"Jennifer";"Sarah";"Max";"Hugo"|]
-
-let a = Normal.Sample(30.00,4.00)
-
-let genUser _ = {firstName = names.[System.Random().Next(0,10)]; age = int(Normal.Sample(30.00,4.00)); height = Normal.Sample(175.00 , 5.0); weight = Normal.Sample(75.00,5.00)}  
-let createUser n =
-  [1..n] |> List.map genUser
-  
+open Generator.Users
 
 
 [<EntryPoint>] 
