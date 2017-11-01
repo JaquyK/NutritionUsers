763ae38cd0f6abf749a14de93a88129491b29991
Jaqueline Kwiasowski <jaqueline@freeletics.com>
2017-11-01 21:10:50 +0100

    added Tests

--- Generator.Test/Program.fs
+++ Generator.Test/Program.fs
@@ -1,8 +1,27 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Expecto
+//open Expecto.FsCheck
+open Generator.Users
+
+
+[<Tests>]
+let testNumberOfUsers =
+    testProperty "number of generated users matches given argument if > 0 and 0 otherwise" <| fun (n: int) -> 
+    (createUser n |> List.length) = if n > 0 then n
+                                    else 0
+
+[<Tests>]
+let testUserParameters =
+    testProperty "generated User has height, weight, age and name" <|fun x ->
+    let athlete = createUser 1
+    athlete.[0].height > 0.0 &&
+    athlete.[0].weight > 0.0 &&
+    athlete.[0].age > 0 &&
+    athlete.[0].firstName.Length > 0 
+
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let main argv = 
+    runTestsInAssembly defaultConfig argv
